0ae213eea1c28901a77bcbea4da48a13896bba2d
cleanaz-dev <founder@cleanazcoding.com>
2025-09-28 09:17:32 -0400

    watermark added to insite image-gen

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -7,7 +7,14 @@ cloudinary.config({
 });
 
 export async function uploadToCloudinary(url: string, folder: string) {
-  const eager = { width: 1024, height: 768, crop: "limit", quality: "auto:good", fetch_format: "jpg" };
+  const eager = { 
+    width: 1024, 
+    height: 768, 
+    crop: "limit", 
+    quality: "auto:good", 
+    fetch_format: "jpg",
+    overlay: "watermark_nvdxw8"
+  };
   const { secure_url } = await cloudinary.uploader.upload(url, { folder, eager: [eager] });
   return secure_url;
 }
