cbb6bf765370def9062bbdd69273331644b02221
cleanaz-dev <founder@cleanazcoding.com>
2025-10-30 12:19:04 -0400

    update landing page video url

--- components/ui/landing-page/hero-section.tsx
+++ components/ui/landing-page/hero-section.tsx
@@ -64,7 +64,7 @@ export function HeroSection() {
               poster={videoThumbnail.src}
             >
               <source
-                src="https://hue-line.s3.us-east-1.amazonaws.com/new-video-01-audio.mp4"
+                src="https://hue-line.s3.us-east-1.amazonaws.com/video.mp4"
                 type="video/mp4"
               />
               Your browser does not support the video tag.
