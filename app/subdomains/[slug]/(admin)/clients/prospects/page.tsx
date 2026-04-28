fc8861fc2b02614c689366b1560f941c9cc1de87
cleanaz-dev <founder@cleanazcoding.com>
2026-04-28 18:02:41 -0400

    prospects page

--- app/subdomains/[slug]/(admin)/clients/prospects/page.tsx
+++ app/subdomains/[slug]/(admin)/clients/prospects/page.tsx
@@ -16,7 +16,7 @@ export default async function ProspectPage() {
   })
 
   return (
-    <div className="container mx-auto py-10">
+    <div className="admin-first-div">
       <div className="flex justify-between items-center mb-6">
         <div>
           <h1 className="text-3xl font-bold tracking-tight">Prospects</h1>
