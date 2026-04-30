d45971130eefb04afb0dda36a7039466920edaf9
cleanaz-dev <founder@cleanazcoding.com>
2026-04-30 12:47:33 -0400

    added email webhook from zoho

--- app/api/webhooks/zoho-mail/route.ts
+++ app/api/webhooks/zoho-mail/route.ts
@@ -1,8 +1,35 @@
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
+
+    const {
+      fromAddress,
+      toAddress,
+      summary,
+      subject,
+      hasAttachment,
+      sender,
+      size,
+    } = body;
+
+    const isDomain = await prisma.subdomainUser.findFirst({
+      where: {
+        email: {
+          contains: toAddress,
+        },
+      },
+    });
+
+    if (!isDomain) {
+      console.log("Not Admin Domain");
+      return NextResponse.json({ message: "Invalid Request" }, { status: 400 });
+    }
+
+    
+
     console.log("Zoho Mail Webhook:", body);
     return NextResponse.json({ message: "Success" }, { status: 200 });
   } catch (error) {
