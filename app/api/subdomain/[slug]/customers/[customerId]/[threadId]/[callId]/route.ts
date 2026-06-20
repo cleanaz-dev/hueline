5f45828ba15bb7a26c818669b4f91e59dfc269cd
CleanAZCoding <founder@cleanazcoding.com>
2026-06-20 12:10:54 -0400

    updates

---
+++ app/api/subdomain/[slug]/customers/[customerId]/[threadId]/[callId]/route.ts
@@ -0,0 +1,38 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+interface Params {
+  params: Promise<{
+    slug: string;
+    customerId: string;
+    threadId: string;
+    callId: string;
+  }>;
+}
+export async function PATCH(req: Request, { params }: Params) {
+  const { callId, customerId, slug, threadId } = await params;
+
+  const authHeaders = req.headers.get("x-webhook-secret");
+
+  try {
+    
+    const body = await req.json();
+    const { duration, status, roomName } = body;
+
+    await prisma.call.update({
+      where: { id: callId },
+      data: {
+        status,
+        duration,
+      },
+    });
+
+    return NextResponse.json({ message: "Updated Call" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Error Updating Call" },
+      { status: 500 },
+    );
+  }
+}
